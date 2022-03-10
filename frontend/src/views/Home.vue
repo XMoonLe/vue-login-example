5ce0e4c6bad203a9e38faae2eacb95cc68dbea89
XMoonLe <xmoonle11@gmail.com>
2022-03-10 15:46:36 -0400

    feat(home): nice welcoming

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,3 +1,29 @@
+<script setup>
+  import {useUserStore} from '../store/user'
+  const userStore = useUserStore()
+
+</script>
+
 <template>
-  <h1>This is the home page!</h1>
+  <main class="home-container">
+    <h1>Welcome, {{userStore.state.username}}</h1>
+    <p>You just logged in.</p>
+  </main>
 </template>
+
+<style scoped>
+  .home-container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: var(--font-secondary);
+    gap: 20px;
+  }
+
+  h1 {
+    font-weight: 400;
+    font-style: italic;
+  }
+</style>
